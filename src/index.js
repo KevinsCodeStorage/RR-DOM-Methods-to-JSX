c7d293c94a6991cebe113e68e74e32a04797cb90
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-01-24 21:25:45 -0500

    partial refactoring, still works

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
-let koalaContainer = document.createElement('div')
-
-koalaContainer.setAttribute('class', 'ui items')
+let koalaContainer = <div className="ui items"></div>
 
 // Koala Card
 // koalas are in './public/koalas.js'
 koalas.forEach(koala => {
-    let koalaCard = document.createElement('div')
-    koalaCard.setAttribute('class', 'item')
+    let koalaCard = <div className='item'></div>
     koalaCard.style.cursor = 'pointer';
 
     // Koala Card Image
-    let imageContainer = document.createElement('div')
-    let koalaImage = document.createElement('img')
+    let imageContainer = <div className='image'><img src= {koala.imageURL}></img></div>
 
-    imageContainer.setAttribute('class', 'image')
-
-    koalaImage.setAttribute('src', koala.imageURL)
-
-    imageContainer.append(koalaImage)
 
     // Koala Card Content
-    let koalaContent = document.createElement('div')
+    let koalaContent = <div></div>
     koalaContent.setAttribute('class', 'content')
 
-    let header = document.createElement('div')
+    let header = <div></div>
     header.setAttribute('class', 'header')
     header.append(koala.name)
 
     let descriptionParagraph = document.createElement('p')
     descriptionParagraph.append(koala.description)
 
-    let descriptionContainer = document.createElement('div')
+    let descriptionContainer = <div></div>
     descriptionContainer.setAttribute('class', 'description')
     descriptionContainer.append(descriptionParagraph)
 
